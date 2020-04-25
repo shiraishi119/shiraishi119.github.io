3cfcbdad4af0718a06f021672c38419b3983100c
shiraishi119 <shiraishikenji119@gmail.com>
2020-04-26 00:04:50 +0900

    edit all

--- asset/f_intro.js
+++ asset/f_intro.js
@@ -1,5 +1,4 @@
 // JavaScript Document
-document.write('<embed src=flash/intro.swf width=780 height=600 type=application/x-shockwave-flash autostart=true loop=true VALUE=transparent wmode=transparent>')
-
-
-
+document.write(
+  "<embed src=flash/intro.swf width=780 height=600 type=application/x-shockwave-flash autostart=true loop=true VALUE=transparent wmode=transparent>"
+);
