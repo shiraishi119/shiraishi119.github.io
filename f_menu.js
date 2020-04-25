f6a7c94154c7805a2d73445dbac13f3331685e30
shiraishi119 <shiraishikenji119@gmail.com>
2020-04-25 23:02:42 +0900

    make folder

--- f_menu.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-document.write('<embed src=flash/menu.swf width=625 height=75 type=application/x-shockwave-flash autostart=true loop=true>')
-
-
-
---
+++ js/f_menu.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+document.write('<embed src=flash/menu.swf width=625 height=75 type=application/x-shockwave-flash autostart=true loop=true>')
+
+
+
